3a6e3f1eb84a3e23d4ea50c826fcf4a33e7de538
julianrowe <julianrowe123@gmail.com>
2022-10-06 12:36:42 -0700

    Uploading initial Java Spring files with our teams HTTP API for Assignment 3 and Jsoup

---
+++ src/main/java/com/example/springtest/parseMovies.java
@@ -0,0 +1,43 @@
+package com.example.springtest;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Controller;
+
+import java.io.IOException;
+
+@Controller
+public class parseMovies{
+    static String movies;
+    public static String getMovies(){
+        StringBuilder str = new StringBuilder();
+        try{
+            Document doc = Jsoup.connect("https://www.google.com").get();
+
+            // get page title
+            String title = doc.title();
+            str.append("title : " + title);
+            // System.out.println("title : " + title);
+
+            // get all links
+            Elements links = doc.select("a[href]");
+            for (Element link : links) {
+
+                // get the value from href attribute
+                // System.out.println("\nlink : " + link.attr("href"));
+                // System.out.println("text : " + link.text());
+
+                str.append("<br>link : " + link.attr("href"));
+                str.append("<br>text : " + link.text());
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        movies = str.toString();
+        return movies;
+    }
+}
